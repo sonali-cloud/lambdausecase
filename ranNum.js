225691a72d501c702a44db887bb8195ec8f6bb4b
Ubuntu <ubuntu@ip-172-31-95-54.ec2.internal>
2019-11-06 12:41:42 +0000

    2nd change in ranNum

--- ranNum.js
+++ ranNum.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   const now = moment().format();
 
-  const message = 'Your dice throw resulted in ' + 
+  const message = 'Your dice throw resulted XXXXXXXXXXXXXX' + 
   randomNumber + ' and was issued at ' + now;
 
   return message;
